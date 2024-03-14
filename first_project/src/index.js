e3d38f17afcf867d632a00db0666f83102a5ffdf
Matteo <matteoamodio17@gmail.com>
2024-03-14 17:28:08 +0100

    File updated

--- first_project/src/index.js
+++ first_project/src/index.js
@@ -23,5 +23,6 @@ Challenge: Project setup
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />)
