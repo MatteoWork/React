a5b1c8d99747f21bb88ad07a6617d954ce550c9b
Matteo <matteoamodio17@gmail.com>
2024-03-18 15:41:55 +0100

    File updated

--- nonstringprops/src/App.js
+++ nonstringprops/src/App.js
@@ -1,41 +1,14 @@
 import React from "react"
 import Joke from "./Joke"
+import JokesData from "./JokesData"
 
 export default function App() {
+    const jokeElements = jokesData.map(joke => {
+        return <Joke setup={joke.setup} punchline={joke.punchline} />
+    })
     return (
         <div>
-            <Joke 
-                punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
-                isPun={true}
-                upvotes={10}
-                downvotes={2}
-                comments={[{author: "", body: "", title: ""}]}
-            />
-            <Joke 
-                setup="I got my daughter a fridge for her birthday." 
-                punchline="I can't wait to see her face light up when she opens it." 
-                isPun={false}
-            />
-            <Joke 
-                setup="How did the hacker escape the police?" 
-                punchline="He just ransomware!"
-                isPun={true}
-            />
-            <Joke 
-                setup="Why don't pirates travel on mountain roads?" 
-                punchline="Scurvy." 
-                isPun={true}
-            />
-            <Joke 
-                setup="Why do bees stay in the hive in the winter?" 
-                punchline="Swarm." 
-                isPun={true}
-            />
-            <Joke 
-                setup="What's the best thing about Switzerland?" 
-                punchline="I don't know, but the flag is a big plus!" 
-                isPun={false}
-            />
+            {jokeElements}
         </div>
     )
 }
