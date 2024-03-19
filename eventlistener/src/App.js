1e543f88154860cdd8ddd7619d01de272682ddaf
Matteo <matteoamodio17@gmail.com>
2024-03-19 12:14:52 +0100

    File updated

--- eventlistener/src/App.js
+++ eventlistener/src/App.js
@@ -1,11 +1,17 @@
 import React from "react"
 
 export default function App() {
-    
+    function handleClick() {
+        console.log("I was clicked")
+    }
+
+    function handleOnMouseHover() {
+        console.log("MouseHover")
+    }
     return (
         <div className="container">
-            <img src="https://picsum.photos/640/360" />
-            <button>Click me</button>
+            <img src="https://picsum.photos/640/360" onMouseHover={handleOnMouseHover}/>
+            <button onClick={handleClick}>Click me</button>
         </div>
     )
 }
