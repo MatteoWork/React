42f77b2c7c2c6e106338e36379fc55bdaa2dcea3
Matteo <matteoamodio17@gmail.com>
2024-03-19 10:49:05 +0100

    Header and Meme js

--- memegenerator/src/App.js
+++ memegenerator/src/App.js
@@ -1,8 +1,12 @@
 import React from "react"
+import Header from "./Header"
+import Meme from "./Meme"
 
-/**
- * Challenge: Build the Header component
- */
 export default function App() {
-    return <h1>Hello world!</h1>
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>    
+    )
 }
---
+++ memegenerator/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function App() {
+    return (
+        <header>
+            <img src="./troll-face.png" className="header--image" alt="trollface"/>
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
