a2f88b59461061d4736dd91b81507ee3027023ba
Matteo <matteoamodio17@gmail.com>
2024-03-19 11:14:33 +0100

    Main updated

--- memegenerator/src/Meme.js
+++ memegenerator/src/Meme.js
@@ -4,9 +4,21 @@ export default function Meme() {
     return(
         <main>
             <form className="form">
-                <input type="text" className="form--input" placeholder="Top text"/>
-                <input type="text" className="form--input" placeholder="Bottom text"/>
-                <button className="form--button">Get a new meme image 🖼</button>
+                <div>
+                    <label>Top Text
+                        <input type="text" 
+                               className="form--input" 
+                               placeholder="Shut up" />
+                    </label>
+                </div>
+                <div>
+                    <label>Bottom Text
+                        <input type="text" 
+                               className="form--input" 
+                               placeholder="and take my money"/>
+                    </label>
+                </div>
+                                <button className="form--button">Get a new meme image 🖼</button>
             </form>
         </main>
     )
