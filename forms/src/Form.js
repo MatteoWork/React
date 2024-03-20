cd21453f6512e0a6e38a90fa1deac5037c320016
Matteo <matteoamodio17@gmail.com>
2024-03-20 12:56:10 +0100

    Added value

--- forms/src/Form.js
+++ forms/src/Form.js
@@ -4,8 +4,6 @@ export default function Form() {
 
     const [formData, setFormData] = React.useState({firstName : "", lastName : "", email : ""})
 
-    console.log(FormData)
-
     function handleChange(event) {
         setFormData(prevFormData => {
             return {
@@ -22,18 +20,21 @@ export default function Form() {
                 placeholder="First Name"
                 onChange={handleChange}
                 name ="firstName"
+                value={formData.firstName}
             />
             <input
                 type="text"
                 placeholder="Last Name"
                 onChange={handleChange}
                 name ="lastName"
+                value={formData.lastName}
             />
             <input
                 type="email"
                 placeholder="Email"
                 onChange={handleChange}
                 name ="email"
+                value={formData.email}
             />
         </form>
     )
