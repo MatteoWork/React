d1c5aea192553b5df6bd7702a6db3f7f23a00c54
Matteo <matteoamodio17@gmail.com>
2024-03-20 12:47:19 +0100

    File updated

--- forms/src/index.js
+++ forms/src/index.js
@@ -1,14 +1,5 @@
-document.getElementById("my-form").addEventListener("submit", function(event) {
-    event.preventDefault()
-    const formElements = event.target.elements
-    const {firstName, lastName} = formElements
-    submitViaAPI({
-        firstName: firstName.value,
-        lastName: lastName.value
-    })
-})
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Form from "./Form"
 
-function submitViaAPI(data) {
-    console.log(data)
-    console.log("Submitted!")
-}
+ReactDOM.createRoot(document.getElementById("root")).render(<Form />)
