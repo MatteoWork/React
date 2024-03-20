bb5365e935ec97ed4fbaa840a5f3b0ec2ab67873
Matteo <matteoamodio17@gmail.com>
2024-03-20 09:30:53 +0100

    Onlick boxes added again in imperative way

--- box/src/App.js
+++ box/src/App.js
@@ -3,12 +3,27 @@ import boxes from "./boxes"
 import Box from "./Box"
 
 export default function App() {
-
-    function toggle(id) {
-        console.log(id)
-    }
     
     const [squares, setSquares] = React.useState(boxes)
+
+    function toggle(id) {
+        setSquares(prevSquares => {
+            const newSquares = []
+            for(let i = 0; i < prevSquares.length; i++) {
+                const currentSquare = prevSquares[i]
+                if(currentSquare.id === id) {
+                    const updateSquare = {
+                        ...currentSquare,
+                        on: !currentSquare.on
+                    }
+                    newSquares.push(updateSquare)
+                }else {
+                    newSquares.push(currentSquare)
+                }
+            }
+            return newSquares
+        })
+    }
     
     const squareElements = squares.map(square => (
         <Box key={square.id} 
