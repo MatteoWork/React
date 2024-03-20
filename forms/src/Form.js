e44011327106e6e1d2165a93fcd930134de486a3
Matteo <matteoamodio17@gmail.com>
2024-03-20 14:53:47 +0100

    Select option

--- forms/src/Form.js
+++ forms/src/Form.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Form() {
 
-    const [formData, setFormData] = React.useState({firstName : "", lastName : "", email : "", comments : "", isFriendly : true, employment : ""})
+    const [formData, setFormData] = React.useState({firstName : "", lastName : "", email : "", comments : "", isFriendly : true, employment : "", favColor : ""})
 
     function handleChange(event) {
         const {name, value, type, checked} = event.target
@@ -91,6 +91,25 @@ export default function Form() {
                 <br />
                 
             </fieldset>
+            <br />
+            
+            <label htmlFor="favColor">What is your favorite color?</label>
+            <br />
+            <select 
+                id="favColor"
+                value={formData.favColor}
+                onChange={handleChange}
+                name="favColor"
+                >
+                <option value="">-- Choose --</option>
+                <option value="red">Red</option>
+                <option value="orange">Orange</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="indigo">Indigo</option>
+                <option value="violet">Violet</option>
+            </select>
         </form>
     )
 }
