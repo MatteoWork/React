5a4b02e01e11570177ea1e8d943a702a843574cd
Matteo <matteoamodio17@gmail.com>
2024-03-20 09:21:23 +0100

    Box id's

--- box/src/App.js
+++ box/src/App.js
@@ -3,10 +3,18 @@ import boxes from "./boxes"
 import Box from "./Box"
 
 export default function App() {
+
+    function toggle(id) {
+        console.log(id)
+    }
+    
     const [squares, setSquares] = React.useState(boxes)
     
     const squareElements = squares.map(square => (
-        <Box key={square.id} on={square.on} />
+        <Box key={square.id} 
+             id={square.id}
+             on={square.on} 
+             toggle={toggle}/>
     ))
     
     return (
