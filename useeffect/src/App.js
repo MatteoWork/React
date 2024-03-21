208449f5def44c92b731a411e481ac0e45fe0fa8
Matteo <matteoamodio17@gmail.com>
2024-03-21 16:11:06 +0100

    File updated

--- useeffect/src/App.js
+++ useeffect/src/App.js
@@ -2,19 +2,20 @@ import React from "react"
 
 export default function App() {
     const [starWarsData, setStarWarsData] = React.useState({})
-    const [count, setCount] = React.useState(0)
+    const [count, setCount] = React.useState(1)
     
-    console.log("Component rendered")
-
-    React.useEffect(() => {
-        console.log("Effect function ran")
+    React.useEffect(function() {
+        console.log("Effect ran")
+        fetch(`https://swapi.dev/api/people/${count}`)
+            .then(res => res.json())
+            .then(data => setStarWarsData(data))
     }, [count])
     
     return (
         <div>
-            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
             <h2>The count is {count}</h2>
-            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         </div>
     )
 }
